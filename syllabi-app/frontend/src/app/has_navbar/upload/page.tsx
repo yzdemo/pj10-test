aa384f2a37afc756570f51b512803ec06bb98a70
Timothy Lou <tmthlou@gmail.com>
2026-02-02 19:44:21 -0800

    test

--- syllabi-app/frontend/src/app/has_navbar/upload/page.tsx
+++ syllabi-app/frontend/src/app/has_navbar/upload/page.tsx
@@ -10,7 +10,7 @@ export default function UploadPage() {
         </h1>
 
         <p className="text-gray-600 text-center mb-6">
-          Please upload a PDF file to continue. Test
+          Please upload a PDF file to continue. Testasdlfjasl;dfjl;asjf;ljas;lfjal;sj
         </p>
 
         <PdfUpload />
