81fd6cde243f933819db4de01f2ae847e5661078
Timothy Lou <tmthlou@gmail.com>
2026-02-02 20:01:46 -0800

    tried changing css

--- syllabi-app/frontend/src/app/has_navbar/upload/page.tsx
+++ syllabi-app/frontend/src/app/has_navbar/upload/page.tsx
@@ -3,14 +3,14 @@ import Navbar from "../../components/navbar";
 
 export default function UploadPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center" style={{ backgroundColor: "#264653"}}>
+    <main className="min-h-screen flex items-center justify-center">
       <div className="max-w-md w-full p-6">
         <h1 className="text-2xl font-semibold mb-4 text-center">
           Upload your PDF
         </h1>
 
         <p className="text-gray-600 text-center mb-6">
-          Please upload a PDF file tasdfljas;ldfjasl;fjdo continue.
+          Please upload a PDF file to continue.
         </p>
 
         <PdfUpload />
